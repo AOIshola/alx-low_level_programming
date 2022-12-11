97dac3e8a1dd7cffc7274ad7e11a1a13639a14ae
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-10 22:32:58 -0800

    print letters but q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'prints alphabets except q and e
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+			if (c != 'q' && c != 'e')
+			{
+				putchar(c);
+			}
+		}
+	putchar('\n');
+	return (0);
+}
