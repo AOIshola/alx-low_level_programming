ea59100c9491d90d1ff26d8c15153affcafc838e
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-10 23:14:51 -0800

    print numbers in base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'print numbers'
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while
+		(n < 10)
+		{
+			printf("%d", n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
