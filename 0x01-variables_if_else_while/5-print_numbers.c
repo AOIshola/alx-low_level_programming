5712d7b06a6681b0c84a88c8fb45259852726ae4
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-10 23:06:01 -0800

    print single-digit numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'print single-digit numbers in base 10'
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while
+		(n < 10)
+		{
+			printf("%d\n", n);
+			n++;
+		}
+	return (0);
+}
