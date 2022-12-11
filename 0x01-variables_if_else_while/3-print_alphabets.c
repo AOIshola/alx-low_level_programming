868cf482a7be3dbd318436a3c7bb514936c7f385
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-10 22:13:19 -0800

    print upper and lowercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'Print upper and lowercase alphabets'
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z')
+		{
+			putchar(d);
+			d++;
+		}
+	return (0);
+}
