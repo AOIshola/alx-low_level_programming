32e9a774fede860048dd20a369dc6ce3a197a4ab
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-13 14:51:17 -0800

    print alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print alphabets 10 times
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int num;
+	char c = 'a';
+
+	for (num = 0; num < 10; num++)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+	}
+	_putchar('\n');
+}
