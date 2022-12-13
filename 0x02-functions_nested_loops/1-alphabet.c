1e86c97b2c2fcf4bfcd991f9b343b5f802c15122
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-13 14:09:54 -0800

    print alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *Description: print alphabets in lowercase
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	for(c <= 'a'; c <= 'z'; c++)
+	{
+		_putchar('c');
+	}
+	_putchar('\n');
+	return (0);
+}
