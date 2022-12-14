54072dc0bc9e26a8c59e5e38abc8e6131b3aec13
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-13 19:35:04 -0800

    check for uppercase characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_isalpha - checks for characters in uppercase
+ *@c: character parameter
+ *Return: returns 1 or 0, depending on the condition
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
