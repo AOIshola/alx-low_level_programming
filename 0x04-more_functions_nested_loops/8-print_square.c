1eae1f61a2c307ba09c295fafe06b0ded61fce40
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-15 15:33:01 -0800

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - print a square 
+ * @size: parameter passed
+ */
+
+void print_square(int size)
+{
+	int i = 0, j;
+
+	if (size > 0)
+	{
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
