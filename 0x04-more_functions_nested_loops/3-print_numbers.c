a3aa5062b801fad112c06982df6fdb3c2338a788
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-15 14:01:14 -0800

    print numbers 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- *print_numbers - print 0 to 9
- *
- *Return: Always 0 (Success)
+ * print_numbers - print numbers 0 to 9
+ * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
 	int c;
 
-	for (c = 48; c < 58; c++)
+	for (c = 0; c < 9; c++)
 	{
 		_putchar(c);
 	}
