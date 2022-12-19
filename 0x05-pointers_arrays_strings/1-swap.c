3fa0e195323b243da42aadf190176686d811dcf2
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-19 12:48:58 -0800

    hot fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,8 @@
 /**
  * swap_int - swap the values of two integers
  * @a: first pointer parameter
- * @b: second pointer parameter
+ * @b: second pointer parameteri
+ * @c: variable to store swap
  * Return: Always 0
  */
 
