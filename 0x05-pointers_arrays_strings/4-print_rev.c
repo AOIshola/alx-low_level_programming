5a59fde21046739cffd09df70f98bca6f19de628
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-19 14:18:59 -0800

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: pointer argument
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
