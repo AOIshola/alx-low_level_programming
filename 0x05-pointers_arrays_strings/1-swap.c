e5a003cc747164d76b7edaecd8309db9d6adf442
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-19 12:59:12 -0800

    hot fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,14 +4,14 @@
 /**
  * swap_int - swap the values of two integers
  * @a: first pointer parameter
- * @b: second pointer parameteri
+ * @b: second pointer parameter
  * int c: variable to store swap
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 
 void swap_int(int *a, int *b)
 {
-	int c = *b;
+	int c = *a;
 	*a = *b;
 	*b = c;
 }
