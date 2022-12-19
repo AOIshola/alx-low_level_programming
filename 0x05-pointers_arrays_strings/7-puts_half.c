281dfe4c928a2ea4b7e969a9bd557c1968c9bf42
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-19 15:24:31 -0800

    print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer argument
+ * Return: Always 0 (Success)
+ */
+
+void puts_half(char *str)
+{
+	int count = 0, i;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+			break;
+		count++;
+	}
+
+	if (count % 2 == 1)
+	{
+		i = count / 2;
+	}
+	else
+	{
+		i = (count - 1) / 2;
+	}
+	for (i++; i < count; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
