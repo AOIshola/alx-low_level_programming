a32bd719e4ed925919eabb37bf21291d9f48cf09
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-19 13:24:36 -0800

    print string and newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a newline
+ * @str: pointer that points to a char datatype
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
