9281bfd524d3148ba6b2757d564c73231f47680a
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-22 13:42:39 -0800

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * _strcmp: compare strings
@@ -6,7 +7,7 @@
  * Return: integer
  */
 
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
 	int counter, cmp;
 
