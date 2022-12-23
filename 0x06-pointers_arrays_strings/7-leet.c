e65d302f3f0a9e7b588b9cabeb1ad0dd4f12ae17
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-22 23:39:32 -0800

    encode a string to 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * leet - encodes a string to 1337
+ * @str: pointer to string to encode
+ * Return: the designated pointer
+ */
+char *leet(char *str)
+{
+	int length = 0, n;
+	int lower[] = {97, 101, 111, 116, 108}
+	int caps[] = {65, 69, 79, 84, 76}
+	int code[] = {52, 51, 48, 55, 49}
+
+	while (*(str + length) != '\0')
+	{
+		for (n = 0; n < 5; n++)
+		{
+			if (*(str + length) == lower[n] || *(str + length) ==
+					caps[n])
+			{
+				*(str + length) = code[n];
+				break;
+			}
+		}
+		length++;
+	}
+
+	return (str);
+}
