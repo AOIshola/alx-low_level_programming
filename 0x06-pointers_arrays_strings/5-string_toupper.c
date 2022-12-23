331923064cb5057a637d0bb5afd83fb70bbb9985
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-22 22:06:59 -0800

    convert to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - convert lowercases to uppercase
+ * @str: string to convert to uppercase
+ * Return: char
+ */
+
+char *string_toupper(char *str);
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		if (str[length] >= 97 && str[length] <= 122)
+		{
+			str[length] = str[length] - 32;
+		}
+		lenght++;
+	}
+
+	return (str);
+}
