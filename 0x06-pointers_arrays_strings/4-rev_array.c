69a86a64822c819bfb2ee0dead007982317da2b6
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2022-12-22 21:39:25 -0800

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,14 +3,14 @@
  * reverse_array - reverses an array of integers
  * @a: array of integers argument
  * @n: number of elements in array
- * Return: Always 0 (Success)
+ * Return: Void
  */
 
 void reverse_array(int *a, int n)
 {
 	int counter, temp;
 
-	n = n -1;
+	n = n - 1;
 	counter = 0;
 	while (counter <= n)
 	{
