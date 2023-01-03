ec29852463af1d4ea21890e835867b37e808464a
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-01-03 00:27:22 -0800

    set value of pointer to a char

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,3 +7,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 char _putchar(char c);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
