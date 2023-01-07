b6c11318a85bf6433b2a17ff5efe3fdc7c2381ea
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-01-07 14:43:58 -0800

    create static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 
 int _islower(int c);
@@ -37,3 +39,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+#endif
