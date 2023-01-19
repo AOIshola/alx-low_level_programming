5ebd763d0bdb68964aae3fc4e8bf44a00f936e8d
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-01-19 12:25:19 -0800

    use function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -19,10 +19,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
 		{
-		return (i);
+			if (cmp(array[i]) != 0)
+			{	
+				return (i);
+			}
 		}
 	}
 	return (-1);
