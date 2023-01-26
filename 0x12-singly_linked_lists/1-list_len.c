c761544a4d5f75446ab4ba2f66b6efa6eb922153
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-01-26 08:12:02 -0800

    use singly-linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a list
+ * @h: singly linked lists
+ * Return: number of elements in the list
+ */
+
+size_t list_len(const list_len *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h =! NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
