1d595e5c0ce170f2c9fefd060a19f899db01cd61
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-01-26 08:54:01 -0800

    hot fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 
-
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: The list_t list.
@@ -19,7 +18,7 @@ size_t print_list(const list_t *h)
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
-	
+
 		nodes++;
 		h = h->next;
 	}
