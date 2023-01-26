e3a32a377c7a90744e7d29770aa1b0d8f1e443fe
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-01-26 09:05:52 -0800

    print number of nodes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
- * Return: The number of nodes in h.
+ * print_list - Prints all the elements of a list_t list
+ * @h: The list_t list
+ * Return: The number of nodes in h
  */
 
 size_t print_list(const list_t *h)
