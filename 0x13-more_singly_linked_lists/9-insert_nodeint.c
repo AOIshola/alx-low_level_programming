1195316cf9d1846762e81536284062e44241ccae
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-02-05 05:53:52 -0800

    insert node at any position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,6 @@
  * @n: The integer data of the new node
  * Return: The address of the new inserted node or NULL if failed
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node, *temp = *head;
