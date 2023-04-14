2362eef5204ece9a2cd38f707eac9893249d2f73
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-04-14 03:14:12 -0700

    implement hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -12,7 +12,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int hash;
 	int c;
 
-	while (c = *str++)
+	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
