b1508eb6f63f4e19696f1ce687405932c148f2b7
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-07-06 07:31:04 +0100

    Delete 0x08-recursion directory

--- 0x08-recursion/main.h
+++
@@ -1,9 +0,0 @@
-void _puts_recursion(char *s);
-char _putchar(char c);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-
