930667fb642f462159ab6d2022d5ea57f5d5a983
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-07-17 00:18:00 -0700

    use betty coding style

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,16 +12,16 @@
 
 void free_grid(int **grid, int height)
 {
-    int i;
+	int i;
 
-    if (grid == NULL)
-    {
-        return;
-    }
+	if (grid == NULL)
+	{
+		return;
+	}
 
-    for (i = 0; i < height; i++)
-    {
-        free (grid[i]);
-    }
-    free(grid);
+	for (i = 0; i < height; i++)
+	{
+		free (grid[i]);
+	}
+	free(grid);
 }
