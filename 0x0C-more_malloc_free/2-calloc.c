a0bb40c58d2a72c3c72a3ed75da35d921337bbc3
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-07-17 00:48:41 -0700

    set allocated memory to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,13 +14,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
 	unsigned int n;
-
-	arr = malloc(nmemb * size);
-
+	
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
+	arr = malloc(nmemb * size);
 
 	if (arr == NULL)
 	{
