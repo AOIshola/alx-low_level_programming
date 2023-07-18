c8296cb041333ca15d1622dada77d58cb1c05a9f
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-07-18 15:00:03 -0700

    0x0E C Structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,6 @@ typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void *free_dog(dog_t *d);
+void free_dog(dog_t *d);
 
 #endif
