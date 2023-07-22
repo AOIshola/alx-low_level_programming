2dd699c534b0080969cb04ff115bccbc189e9830
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-07-22 04:43:43 -0700

    hot fix 2-int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0, search;
 
-	if (size <= 0)
-	{
-		return (-1);
-	}
-	
 	if (array != NULL || cmp != NULL)
 	{
 		while (i < size)
