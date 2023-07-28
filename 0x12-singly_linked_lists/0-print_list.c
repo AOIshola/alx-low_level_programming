0b43f8220f6c995f9666b0cab744fb137d697605
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-07-28 01:22:37 -0700

    use singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,10 +13,6 @@ size_t print_list(const list_t *h)
 	unsigned int count = 0;
 	const list_t *temp = h;
 
-	if (h == NULL)
-	{
-		return (NULL);
-	}
 	while (temp != NULL)
 	{
 		if (temp->str != NULL)
