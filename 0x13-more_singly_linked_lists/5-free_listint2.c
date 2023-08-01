1de15c82e0b7eb5ff19821c72f9c0821bf17054a
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-08-01 04:05:24 -0700

    use more singly linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,6 +12,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (*head == NULL)
+	{
+		return;
+	}
 	while (*head != NULL)
 	{
 		temp = *head;
@@ -19,5 +23,5 @@ void free_listint2(listint_t **head)
 		free(temp);
 	}
 
-	head = NULL;
+	*head = NULL;
 }
