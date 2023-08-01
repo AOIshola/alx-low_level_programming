7256e8552233c4d996a7f2aa6138e855c7f3dd0f
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-08-01 03:40:35 -0700

    use more singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
+	size_t count = 0;
 	const listint_t *currentPtr = h;
 
 	while (currentPtr != NULL)
 	{
+		count++;
 		printf("%d\n", currentPtr->n);
 		currentPtr = currentPtr->next;
-		count++;
 	}
 
 	return (count);
