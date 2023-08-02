60e7f98a409585f430c596b1c350b154d97154e6
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-08-02 03:37:11 -0700

    use more singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newNode->next = NULL;
 		prevPtr = NULL;
 		currentPtr = *head;
-		if (head == NULL && !idx)
+		if (head == NULL)
 		{
 			*head = newNode;
 			return (newNode);
