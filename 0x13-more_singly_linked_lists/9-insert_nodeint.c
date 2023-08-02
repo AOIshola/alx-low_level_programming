2c989ffd1490cdc35458648a0c7e7c3fab62b20b
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-08-01 20:09:21 -0700

    use more singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,15 +17,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode = malloc(sizeof(*newNode));
 	unsigned int i = 0;
 
-	if (head == NULL)
-		return (NULL);
 	if (newNode != NULL)
 	{
 		newNode->n = n;
 		newNode->next = NULL;
 		prevPtr = NULL;
 		currentPtr = *head;
-
+		if (head == NULL)
+		{
+			*head = newNode;
+			return (newNode);
+		}
 		while (currentPtr->next != NULL && i != idx)
 		{
 			prevPtr = currentPtr;
