3af49cba027d56b4da84ddae1197f970b19faea9
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-08-31 06:30:59 -0700

    manipulate bits

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,13 +6,13 @@
  * Return: 0 if Big Endian, 1 if Little Endian
  */
 
-int get_endianess(void)
+int get_endianness(void)
 {
 	unsigned int i = 1;
 	char *c;
 
 	c = (char *)&i;
-	if (*c)
+	if (!*c)
 		return (1);
 	else
 		return (0);
