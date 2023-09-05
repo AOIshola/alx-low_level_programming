50f77f97cc9a6ab08bd3f9eebaf6e779e64ff2b6
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-09-05 03:32:27 -0700

    use file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,12 +13,12 @@ void check_error(ssize_t f_from, ssize_t f_to, char *argv[])
 {
 	if (f_from < 0)
 	{
-		dprintf(STDERR_FILENO, "%s %s\n", "Error: Can't read from", argv[1]);
+		dprintf(STDERR_FILENO, "%s %s\n", "Error: Can't read from file", argv[1]);
 		exit(98);
 	}
 	if (f_to < 0)
 	{
-		dprintf(STDERR_FILENO, "%s %s\n", "Error: Can't write to file", argv[2]);
+		dprintf(STDERR_FILENO, "%s %s\n", "Error: Can't write to", argv[2]);
 		exit(99);
 	}
 }
