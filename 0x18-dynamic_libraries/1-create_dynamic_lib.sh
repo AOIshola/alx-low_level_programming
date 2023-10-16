4ce08f60bab2bcd65c45b38e957ece391a18d599
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-10-16 09:58:52 -0700

    create dynamic C libraries

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gcc -c -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+
---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int _isupper(int c);
+char *_memset(char *s, char b, unsigned int n);
+char *_strcat(char *dest, char *src);
+int _isdigit(int c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strchr(char *s, char c);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+int _islower(int c);
+int _strcmp(char *s1, char *s2);
+unsigned int _strspn(char *s, char *accept);
+int _isalpha(int c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _abs(int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+void _puts(char *str);
+int _putchar(char c);
+
+#endif
