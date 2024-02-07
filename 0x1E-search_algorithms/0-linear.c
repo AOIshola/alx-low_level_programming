010312b4a639cb7baeb8f22777e29d367f20adcf
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-02-07 14:57:50 -0800

    implement linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
